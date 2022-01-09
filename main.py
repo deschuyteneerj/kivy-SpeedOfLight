89e66a0d5913aca1bceeb1d2755fa19fac8e3d65
Jonathan <deschuyteneerj@gmail.com>
2022-01-09 01:27:41 +0100

    horizontal_lines

--- main.py
+++ main.py
@@ -9,19 +9,25 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 10
-    V_LINES_SPACING = .2  # screen width percentage
+    V_LINES_SPACING = .1  # screen width percentage
     vertical_lines = []
+
+    H_NB_LINES = 15
+    H_LINES_SPACING = .2  # screen height percentage
+    horizontal_lines = []
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         # print(' INIT W:' + str(self.width) + ' H:' + str(self.height))
         self.init_vertical_lines()
+        self.init_horizontal_lines()
 
     def on_parent(self, widget, parent):
         print('ON PARENT W:' + str(self.width) + ' H:' + str(self.height))
 
     def on_size(self, *args):
         self.update_vertical_lines()
+        self.update_horizontal_lines()
         # print('ON SIZE W:' + str(self.width) + ' H:' + str(self.height))
         # self.perspective_point_x = self.width / 2
         # self.perspective_point_y = self.height * 0.75
@@ -37,7 +43,6 @@ class MainWidget(Widget):
     def init_vertical_lines(self):
         with self.canvas:
             Color(1, 1, 1)
-            # self.line = Line(points=[100, 0, 100, 100])
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
@@ -48,16 +53,36 @@ class MainWidget(Widget):
         for i in range(0, self.V_NB_LINES):
             line_x = int(central_line_x + offset * spacing)
             x1, y1 = self.transform(line_x, 0)
-            x2, y2 = self.transform(line_x, self.height)  # int(self.height) ???
+            x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
 
+    def init_horizontal_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line())
+
+    def update_horizontal_lines(self):
+        central_line_x = self.width / 2
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES / 2) + 0.5
+        xmin = central_line_x + offset * spacing
+        xmax = central_line_x - offset * spacing
+        spacing_y = self.H_LINES_SPACING * self.height
+        for i in range(0, self.H_NB_LINES):
+            line_y = i * spacing_y
+            x1, y1 = self.transform(xmin, line_y)
+            x2, y2 = self.transform(xmax, line_y)
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
+
+    # Function to change perspective from 2D to vanishing point perspective
     def transform(self, x, y):
         # return self.transform_2d(x, y)
         return self.transform_perspective(x, y)
 
     def transform_2d(self, x, y):
-        return x, y
+        return int(x), int(y)
 
     def transform_perspective(self, x, y):
         tr_y = y * self.perspective_point_y / self.height
@@ -69,7 +94,7 @@ class MainWidget(Widget):
         offset_x = diff_x * diff_y / self.perspective_point_y
 
         tr_x = self.perspective_point_x + offset_x
-        return tr_x, tr_y
+        return int(tr_x), int(tr_y)
 
 
 class SpeedOfLightApp(App):
