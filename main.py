f84c210fc2a76614d92d7c8f11b3bd408bf27919
Jonathan <deschuyteneerj@gmail.com>
2022-01-09 14:58:10 +0100

    refactor_code

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from kivy.core.window import Window
 
 
 class MainWidget(Widget):
+    from transforms import transform, transform_2d, transform_perspective
+    from user_actions import keyboard_closed, on_keyboard_up, on_keyboard_down, on_touch_up, on_touch_down
     # Vanishing point
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
@@ -46,44 +48,10 @@ class MainWidget(Widget):
             self.keyboard.bind(on_key_up=self.on_keyboard_up)
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
-    def keyboard_closed(self):
-        self.keyboard.unbind(on_key_down=self.on_keyboard_down)
-        self.keyboard = None
-
     def is_desktop(self):
         if platform in ('linux', 'win', 'macosx'):
             return True
         return False
-
-    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        if keycode[1] == 'left':
-            self.current_speed_x = self.SPEED_X
-        elif keycode[1] == 'right':
-            self.current_speed_x = -self.SPEED_X
-        return True
-
-    def on_keyboard_up(self, keyboard, keycode):
-        self.current_speed_x = 0
-
-    def on_parent(self, widget, parent):
-        pass
-        # print('ON PARENT W:' + str(self.width) + ' H:' + str(self.height))
-
-    def on_size(self, *args):
-        pass
-        # self.update_vertical_lines()
-        # self.update_horizontal_lines()
-        # print('ON SIZE W:' + str(self.width) + ' H:' + str(self.height))
-        # self.perspective_point_x = self.width / 2
-        # self.perspective_point_y = self.height * 0.75
-
-    def on_perspective_point_x(self, widget, value):
-        # print('PX: ' + str(value))
-        pass
-
-    def on_perspective_point_y(self, widget, value):
-        # print('PY: ' + str(value))
-        pass
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -121,39 +89,6 @@ class MainWidget(Widget):
             x1, y1 = self.transform(xmin, line_y)
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
-
-    # Function to change perspective from 2D to vanishing point perspective
-    def transform(self, x, y):
-        # return self.transform_2d(x, y)
-        return self.transform_perspective(x, y)
-
-    def transform_2d(self, x, y):
-        return int(x), int(y)
-
-    def transform_perspective(self, x, y):
-        lin_y = y * self.perspective_point_y / self.height
-        if lin_y > self.perspective_point_y:
-            lin_y = self.perspective_point_y
-
-        diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - lin_y
-        factor_y = diff_y / self.perspective_point_y
-        factor_y = pow(factor_y, 4)
-
-        offset_x = diff_x * factor_y
-
-        tr_x = self.perspective_point_x + offset_x
-        tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
-        return int(tr_x), int(tr_y)
-
-    def on_touch_down(self, touch):
-        if touch.x < self.width / 2:
-            self.current_speed_x = self.SPEED_X
-        else:
-            self.current_speed_x = -self.SPEED_X
-
-    def on_touch_up(self, touch):
-        self.current_speed_x = 0
 
     # Function refresh 60 fps
     def update(self, dt):
