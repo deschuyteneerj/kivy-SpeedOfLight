66245ee5d9cb55370318bfcae010cfc511049514
Jonathan <deschuyteneerj@gmail.com>
2022-01-09 01:41:19 +0100

    horizontal_lines_spacing

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 10
-    V_LINES_SPACING = .1  # screen width percentage
+    V_LINES_SPACING = .25  # screen width percentage
     vertical_lines = []
 
     H_NB_LINES = 15
-    H_LINES_SPACING = .2  # screen height percentage
+    H_LINES_SPACING = .1  # screen height percentage
     horizontal_lines = []
 
     def __init__(self, **kwargs):
@@ -85,15 +85,19 @@ class MainWidget(Widget):
         return int(x), int(y)
 
     def transform_perspective(self, x, y):
-        tr_y = y * self.perspective_point_y / self.height
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        lin_y = y * self.perspective_point_y / self.height
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
 
         diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - tr_y
-        offset_x = diff_x * diff_y / self.perspective_point_y
+        diff_y = self.perspective_point_y - lin_y
+        factor_y = diff_y / self.perspective_point_y
+        factor_y = pow(factor_y, 4)
+
+        offset_x = diff_x * factor_y
 
         tr_x = self.perspective_point_x + offset_x
+        tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
         return int(tr_x), int(tr_y)
 
 
